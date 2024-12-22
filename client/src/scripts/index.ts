801833c7d6422f18d813edeaec52ff80572a1b75
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2024-12-22 17:41:07 +0500

    Add: shop all and detail views.
    This commit will add list of count f shops, products and purchases via
    script to home page view.
    This commit will also add html and script for all shops view.
    This commit will also add html and script for single shop detail view.
    This commit will also move fetchData function to a module.
    This commit will also move nav bar view to a module.

--- client/src/scripts/index.ts
+++ client/src/scripts/index.ts
@@ -1,55 +1,33 @@
 import "../css/style.css";
 import { HomeResponse } from "@dataTypes/home.types";
+import { createNav } from "./utils/nav";
+import { fetchData } from "./utils/fetchData";
 
-const fetchData = async (url: string): Promise<HomeResponse> => {
-  try {
-    const dataPromise = await fetch(url, {
-      credentials: "include",
-    });
-    if (!dataPromise.ok) {
-      throw new Error(dataPromise.statusText);
-    }
-    const data = await dataPromise.json();
-    if (!data) {
-      throw new Error("No data found for main page");
-    }
-    return data;
-  } catch (err) {
-    console.error(err);
-    return {
-      title: "Unable to fetch Home Page",
-    };
+const homePageData: HomeResponse = await fetchData("http://localhost:3000/");
+const title = document.querySelector("#title") ?? document.createElement("h1");
+title.textContent = homePageData.title;
+// append nav elem
+const nav = createNav();
+document.querySelector("#title")?.insertAdjacentElement("afterend", nav);
+const homePageDataKeys = Object.keys(homePageData);
+let idx = homePageDataKeys.length;
+while (idx > 0) {
+  const k = homePageDataKeys[homePageDataKeys.length - idx];
+  const li = document.createElement("li");
+  switch (k) {
+    case "countOfShops":
+      li.textContent = `Shops: ${homePageData[k]}`;
+      break;
+    case "countOfProducts":
+      li.textContent = `Products: ${homePageData[k]}`;
+      break;
+    case "countOfPurchases":
+      li.textContent = `Purchases: ${homePageData[k]}`;
+      break;
+    default:
+      break;
   }
-};
-
-const main = async () => {
-  const homePageData = await fetchData("http://localhost:3000/");
-  const title =
-    document.querySelector("#title") ?? document.createElement("h1#title");
-  title.textContent = homePageData.title;
-
-  const homePageDataKeys = Object.keys(homePageData);
-  let idx = 0;
-  while (idx < homePageDataKeys.length) {
-    const k = homePageDataKeys[idx];
-    const li = document.createElement("li");
-    switch (k) {
-      default:
-        break;
-      case "countOfShops":
-        li.textContent = `Shops: ${homePageData[k]}`;
-        break;
-      case "countOfProducts":
-        li.textContent = `Products: ${homePageData[k]}`;
-        break;
-      case "countOfPurchases":
-        li.textContent = `Purchases: ${homePageData[k]}`;
-        break;
-    }
-    // if li is not empty, append to #record-counts
-    li.textContent && document.querySelector("#record-counts")?.appendChild(li);
-    idx++;
-  }
-};
-
-main();
+  // if li is not empty, append to #record-counts
+  li.textContent && document.querySelector("#record-counts")?.appendChild(li);
+  idx--;
+}
