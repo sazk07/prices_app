9973a429240d108dd15c4d66ad659eaf6aaf4fb5
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2024-12-27 23:22:07 +0500

    Fix: client side shop create function
    This commit will fix the redirect on submit form on client side

--- client/src/scripts/shop/create.ts
+++ client/src/scripts/shop/create.ts
@@ -1,23 +1,30 @@
 import "@/css/style.css";
 
-const form = document.querySelector("form");
-const shopName = form?.elements.namedItem("shopName") as HTMLInputElement;
-const shopNameVal = shopName.value;
-const shopLocation = form?.elements.namedItem("shopLocation") as HTMLInputElement;
-const shopLocationVal = shopLocation.value;
-const data = {
-  shopName: shopNameVal,
-  shopLocation: shopLocationVal,
-};
-const button = document.querySelector("button");
-
-button?.addEventListener("submit", async () => {
-  await fetch("http://localhost:3000/shop/create", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-  window.location.href = "/pages/shop/list.html";
+const form = document.querySelector("form") as HTMLFormElement;
+form?.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const form = e.target as HTMLFormElement;
+  const data = {
+    shopName: form["shopName"].value.trim(),
+    shopLocation: form["shopLocation"].value.trim(),
+  };
+  try {
+    const response = await fetch("http://localhost:3000/shop/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) {
+      const error = await response.json();
+      console.error("Error:", error);
+      alert("Failed to add shop. Please try again.");
+      return;
+    }
+    window.location.href = "http://localhost:5173";
+  } catch (err) {
+    console.error("Network Error:", err);
+    alert("A network error occurred. Please try again");
+  }
 });
