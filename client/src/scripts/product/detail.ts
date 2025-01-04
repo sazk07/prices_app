69abdddb90cae8cf0eb83ce4ab75e400a8eda367
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2025-01-04 05:39:43 +0500

    Add: product detail view

---
+++ client/src/scripts/product/detail.ts
@@ -0,0 +1,38 @@
+import "@/css/style.css";
+import { createNav } from "../utils/nav";
+import { ProductOutput } from "@dataTypes/product.types";
+import { getData } from "../utils/fetchData";
+
+const productId = new URLSearchParams(window.location.search).get("productId");
+const productData: ProductOutput = await getData(
+  `http://localhost:3000/product/${productId}`,
+);
+
+// NAV
+const nav = createNav();
+document.querySelector("body")?.insertAdjacentElement("afterbegin", nav);
+
+const title =
+  document.querySelector("title") ?? document.createElement("title");
+title.textContent = `${productData.productName} - Expense Tracker`;
+
+const id = document.querySelector("#productId") as HTMLHeadingElement;
+id.textContent = `ID: ${productData.productId}`;
+
+const name = document.querySelector("#productName") as HTMLParagraphElement;
+name.textContent = `Product Name: ${productData.productName}`;
+
+const brand = document.querySelector("#productBrand") as HTMLParagraphElement;
+brand.textContent = `Product Brand: ${productData.productBrand}`;
+
+const category = document.querySelector(
+  "#productCategory",
+) as HTMLParagraphElement;
+category.textContent = `Product Category: ${productData.productCategory}`;
+
+const updateAnchor = document.createElement("a");
+updateAnchor.href = `../../../product/update.html?productId=${productData.productId}`;
+updateAnchor.textContent = "Update Product";
+document
+  .querySelector("#productCategory")
+  ?.insertAdjacentElement("afterend", updateAnchor);
--- client/src/scripts/shop/detail.ts
+++ client/src/scripts/shop/detail.ts
@@ -3,22 +3,29 @@ import "@/css/style.css";
 import { getData } from "@/scripts/utils/fetchData";
 import { createNav } from "@/scripts/utils/nav";
 
-const nav = createNav();
-document.querySelector("body")?.insertAdjacentElement("afterbegin", nav);
 // get shopId from query param in browser url
 const shopId = new URLSearchParams(window.location.search).get("shopId");
 const shopData: ShopOutput = await getData(
   `http://localhost:3000/shop/${shopId}`,
 );
+
+// NAV
+const nav = createNav();
+document.querySelector("body")?.insertAdjacentElement("afterbegin", nav);
+
 const title =
   document.querySelector("title") ?? document.createElement("title");
 title.textContent = `${shopData.shopName} - Expense Tracker`;
+
 const id = document.querySelector("#shopId") as HTMLHeadingElement;
 id.textContent = `ID: ${shopData.shopId}`;
+
 const name = document.querySelector("#shopName") as HTMLParagraphElement;
 name.textContent = `Shop Name: ${shopData.shopName}`;
+
 const location = document.querySelector("#location") as HTMLParagraphElement;
 location.textContent = `Location: ${shopData.shopLocation}`;
+
 const updateAnchor = document.createElement("a");
 updateAnchor.href = `../../../shop/update.html?shopId=${shopData.shopId}`;
 updateAnchor.textContent = "Update Shop";
